d242e83582884dda6d2fc825d5fe2751eed9b120
Bidgi <64425135+Bidgi@users.noreply.github.com>
2020-11-23 10:04:35 +0100

    creation class ville

---
+++ Scance_23112020/Model/Ville.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scance_23112020.Model
+{
+    class Ville
+    {
+        #region Attributs
+        private string _id;
+        private string _nom;
+        private int _codepostal;
+
+
+
+        #endregion
+        #region Constructeur
+        public Ville(string id, string nom, int codepostal)
+        {
+            Id = id;
+            Nom = nom;
+            Codepostal = codepostal;
+        }
+
+        #endregion
+        #region Getters-Setters
+        public string Id { get => _id; set => _id = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public int Codepostal { get => _codepostal; set => _codepostal = value; }
+        #endregion
+        #region Méthodes
+        #endregion
+    }
+}
